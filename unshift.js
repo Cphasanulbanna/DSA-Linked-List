2680779ff2edeb89f7ba153b500a1ebd8c20c5ce
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-03-10 21:36:34 +0530

    unshift

---
+++ unshift.js
@@ -0,0 +1,32 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = newNode;
+    this.length = 1;
+  }
+
+  unshift(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+console.log(new LinkedList(4));
+const myLinkedList = new LinkedList(4);
+console.log(myLinkedList.unshift(6));
