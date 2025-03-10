18064d83ef031b1cb2f91aee5d48b848967f31d1
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-03-10 21:36:28 +0530

    shift

---
+++ shift.js
@@ -0,0 +1,31 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = newNode;
+    this.length = 1;
+  }
+
+  shift() {
+    if (this.length === 0) return undefined;
+    let temp = this.head;
+    this.head = this.head.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    temp.next = null;
+    return temp;
+  }
+}
+
+console.log(new LinkedList(4));
+const myLinkedList = new LinkedList(4);
+console.log(myLinkedList.shift());
